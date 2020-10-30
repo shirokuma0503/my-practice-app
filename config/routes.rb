12e6c698995b832069c97192cb72a1c8439cd147
onimuraaika <aika.onimura@gmail.com>
2020-10-30 11:12:33 +0000

    rubocop-airbnbのgemを追加、コード違反の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    devise_for :users
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    
-    root to: 'homes#top'
-    get 'about' => 'homes#about'
-
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
 end
